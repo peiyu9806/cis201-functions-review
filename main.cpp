32a4cd6df587c25024c2919a4966313841c382c1
pei_yu kuo <pei_yukuo@cis.vvc.edu>
2018-10-16 18:37:52 -0700

    update

--- main.cpp
+++ main.cpp
@@ -1,12 +1,29 @@
 #include<iostream>
-
+#include<string>
 
 using namespace std;
 
 
+
+
+int a_counts(char s)
+{
+    int count = 0;
+    char c = 'a';
+    if(s.at(0) == 'a')
+    {
+        for(i = 0; i <= s.length(s); i++)
+        {
+            count ++;
+        }
+    }
+    return count;
+}
 int main()
 {
-    cout << "Hello World"<< endl;
+    string s = "abca";
+     
+    cout << "There are " << a_counts(s) << "counts of a in the string." << endl;
 
     return 0;
 }
