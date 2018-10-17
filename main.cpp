a3815a3614bb4dbab16d162db5635abc71fece89
peiyu <42792573+peiyu9806@users.noreply.github.com>
2018-10-16 19:31:49 -0700

    Update

--- main.cpp
+++ main.cpp
@@ -1,29 +1,43 @@
+/**
+File: main.cpp
+Description: Prompt the user for a string, and the program will calculate how many the letter a appears in that string and print the result.
+Author: Pei-Yu Kuo
+Email: peiyukuo9810@student.vvc.edu
+Date Created: 10-16-2018
+**/
 #include<iostream>
 #include<string>
 
 using namespace std;
 
-
-
-
-int a_counts(char s)
+/**
+   Will calculate the times "a" appears in the string
+   @param s the the input 
+   @return the counts of as
+*/ 
+int a_counts(string s)
 {
     int count = 0;
-    char c = 'a';
-    if(s.at(0) == 'a')
+    
+    for (int i = 0; i < s.length(); i++)    // loop to print each character of the string. 
     {
-        for(i = 0; i <= s.length(s); i++)
+        char c = s.at(i);   // assign c to the ASCII code of whatever the character at i position of the string 
+        
+        if(c == 'a' || c == 'A')    // if c is equal to the ASCII code of a or A, the count increases
         {
             count ++;
         }
     }
     return count;
 }
+
 int main()
 {
-    string s = "abca";
-     
-    cout << "There are " << a_counts(s) << "counts of a in the string." << endl;
-
+    string input;
+    cout << "Please enter anything.";
+    getline(cin,input);
+   
+    cout << "There are " << a_counts(input) << " counts of 'a's in the string" << endl;
+    
     return 0;
 }
